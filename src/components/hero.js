377879ac1df0f113429416942108b7d67fd7af4a
SAHIL-NAZIR <sahilnazirlone26@gmail.com>
2022-07-03 17:57:03 +0530

    Featured logos added in hero.

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,58 +1,67 @@
 import { ButtonPrimary, ButtonSecondary } from './buttons';
 import FeaturedNft from './featuredNft';
+import Image from 'next/image';
 
 const Hero = () => {
   return (
-    <div className="container flex flex-row items-center h-screen mx-auto">
-      <div className="w-1/2">
-        <h1 className="leading-normal text-[#F5FBF2] text-7xl">
-          Explore, Buy and <br />
-          Sell the
-          <span className="text-primary">
-            {" "}
-            Best <br />
-            NFTs!
-          </span>
-        </h1>
-        <div className="flex flex-row mt-20">
-          <div className="">
-            <ButtonPrimary text="Explore" link="/#" />
+    <div className="container mx-auto">
+      <div className="flex flex-row items-center h-screen mx-auto">
+        <div className="w-1/2">
+          <h1 className="leading-normal text-[#F5FBF2] text-7xl">
+            Explore, Buy and <br />
+            Sell the
+            <span className="text-primary">
+              {" "}
+              Best <br />
+              NFTs!
+            </span>
+          </h1>
+          <div className="flex flex-row mt-20">
+            <div className="">
+              <ButtonPrimary text="Explore" link="/#" />
+            </div>
+            <div className="ml-12">
+              <ButtonSecondary text="Create" link="/#" />
+            </div>
           </div>
-          <div className="ml-12">
-            <ButtonSecondary text="Create" link="/#" />
+          <div className="flex flex-row items-center mt-28">
+            <div>
+              <span className="block text-6xl font-bold font-oxanium text-[#F5FBF2]">
+                32k+
+              </span>
+              <span className="block text-2xl text-[#9B9E9A]">Artworks</span>
+            </div>
+            <div className="mx-24">
+              <span className="block text-6xl font-bold font-oxanium text-[#F5FBF2]">
+                20k+
+              </span>
+              <span className="block text-2xl text-[#9B9E9A]">Auctions</span>
+            </div>
+            <div>
+              <span className="block text-6xl font-bold font-oxanium text-[#F5FBF2]">
+                12k+
+              </span>
+              <span className="block text-2xl text-[#9B9E9A]">Creators</span>
+            </div>
           </div>
         </div>
-        <div className="flex flex-row items-center mt-28">
-          <div>
-            <span className="block text-6xl font-bold font-oxanium text-[#F5FBF2]">
-              32k+
-            </span>
-            <span className="block text-2xl text-[#9B9E9A]">Artworks</span>
-          </div>
-          <div className="mx-24">
-            <span className="block text-6xl font-bold font-oxanium text-[#F5FBF2]">
-              20k+
-            </span>
-            <span className="block text-2xl text-[#9B9E9A]">Auctions</span>
-          </div>
-          <div>
-            <span className="block text-6xl font-bold font-oxanium text-[#F5FBF2]">
-              12k+
-            </span>
-            <span className="block text-2xl text-[#9B9E9A]">Creators</span>
-          </div>
+        <div className="z-10 flex items-center justify-center w-1/2 h-full">
+          <FeaturedNft
+            artistName="Artist"
+            artistId="wzard"
+            nftImage="/temp/featured.jpg"
+            nftValue="0.99"
+            time="25 hrs"
+          />
         </div>
+        <div className="absolute top-0 right-0 w-1/2 h-full bg-[#4f464e45] backdrop-blur-[30px]"></div>
       </div>
-      <div className="z-10 flex items-center justify-center w-1/2 h-full">
-        <FeaturedNft
-          artistName="Artist"
-          artistId="wzard"
-          nftImage="/temp/featured.jpg"
-          nftValue="0.99"
-          time="25 hrs"
-        />
+      <div className="flex flex-row items-center justify-between w-full max-w-[1235px] mx-auto mt-44 mb-24">
+        <Image src={"/assets/coinbase.svg"} width="100%" height={"40px%"} />
+        <Image src={"/assets/blockchain.svg"} width="180%" height={"40px"} />
+        <Image src={"/assets/binance.svg"} width="140%" height={"40px"} />
+        <Image src={"/assets/bitmex.svg"} width="100%" height={"40px"} />
       </div>
-      <div className="absolute top-0 right-0 w-1/2 h-full bg-[#4f464e45] backdrop-blur-[30px]"></div>
     </div>
   );
 }
